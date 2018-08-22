f702a8db168cb6bc86337273c1dee74154eae64c
Stephanie <stephcshea@gmail.com>
2018-08-22 18:13:35 -0400

    initial commit - delete and add working

---
+++ src/Todos.js
@@ -0,0 +1,23 @@
+//functional component --not state that is in app.js
+import React from 'react'
+
+const Todos = ({todos, deleteTodo}) => {
+  const todoList = todos.length ? (
+    todos.map(todo => {
+      return (
+        <div className="collection-item" key={todo.id}>
+          <span onClick={() =>{deleteTodo(todo.id)}}>{todo.content}</span>
+        </div>
+      )
+    })
+  ) : (
+   <p className="center">there are no todos</p>
+  )
+  return(
+    <div className="todos collection">
+      {todoList}
+    </div>
+  )
+}
+
+export default Todos;
